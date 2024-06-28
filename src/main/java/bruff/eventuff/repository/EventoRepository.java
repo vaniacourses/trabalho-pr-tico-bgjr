dcc81ea1e0e3de40d34b6d528500f1e71fdf76c5
GabiCasini <gabrielacasini@id.uff.br>
2024-06-28 00:55:37 -0300

    adicionei bootstrap pra fazer o front
    testei a conexao do bd e tem algo errado de fato. vou estudar pra entender direito oq é ainda, se souberem me avisem (seria legal ter a main funcionando certinho pra gnt trabalhar em branches dps

--- src/main/java/bruff/eventuff/repository/EventoRepository.java
+++ src/main/java/bruff/eventuff/repository/EventoRepository.java
@@ -2,6 +2,8 @@ package bruff.eventuff.repository;
 
 import bruff.eventuff.model.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
 }
