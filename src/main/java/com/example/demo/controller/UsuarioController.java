659bf761dae843faa5caf8d20ecfdf747dd91ebb
GabiCasini <gabrielacasini@id.uff.br>
2024-07-01 22:11:37 -0300

    atualizacoes nas telas de aluno e coord

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Evento;
 import com.example.demo.model.UserAuth;
 import com.example.demo.model.Usuario;
+import com.example.demo.service.EventoService;
 import com.example.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -18,19 +22,27 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private EventoService eventoService;
+
     @PostMapping("/auth")
-    public String autenticaUsuario(UserAuth userAuth, RedirectAttributes attributes){
+    public String autenticaUsuario(UserAuth userAuth, RedirectAttributes attributes, Model model){
         Usuario usuario = usuarioService.autenticaUsuario(userAuth.getLogin(), userAuth.getPassword());
         if (usuario != null){
-            Long id = usuario.getId(); //como usar id?
-            if (usuario.getRole().equals("aluno")){
-                return "aluno/catalogoAluno";
-            } else if (usuario.getRole().equals("coord")) {
-                return "coord/catalogoCoord";
-            }
+            List<Evento> eventos = eventoService.getAllEventos();
+            return indexAuth(model, usuario, eventos);
         }
         attributes.addFlashAttribute("authError", "Usuário não encontrado :(");
         return "redirect:/loginPage";
+    }
+
+    public String indexAuth (Model model, Usuario usuario, List<Evento> eventos){
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("eventos", eventos);
+        if (usuario.getRole().equals("aluno")){
+            return "aluno/catalogoAluno";
+        }
+        return "coord/catalogoCoord";
     }
 
     @RequestMapping("/auth/listar")
